ed4251a78c952326b8cf7261bc418c3bf0ad6004
KeresztesHunor <kereszteshunor36@gmail.com>
2024-01-09 05:59:45 +0100

    initial commit

---
+++ Vector2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vectors
+{
+    struct Vector2<T> : IEquatable<Vector2<T>> where T : struct, IComparable, IComparable<T>, IConvertible, IEquatable<T>, IFormattable
+    {
+        public T x { get; }
+        public T y { get; }
+
+        public Vector2<T> yx { get => new Vector2<T>(y, x); }
+
+        public Vector2(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public bool Equals(Vector2<T> other)
+        {
+            return this == other;
+        }
+
+        public static bool operator ==(Vector2<T> left, Vector2<T> right) => left.x.Equals(right.x) && left.y.Equals(right.y);
+        public static bool operator !=(Vector2<T> left, Vector2<T> right) => !left.x.Equals(right.x) || !left.y.Equals(right.y);
+    }
+}
---
+++ Vector3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vectors
+{
+    struct Vector3<T> : IEquatable<Vector3<T>> where T : struct, IComparable, IComparable<T>, IConvertible, IEquatable<T>, IFormattable
+    {
+        public T x { get => xy.x; }
+        public T y { get => xy.y; }
+        public T z { get; }
+
+        public Vector2<T> xy { get; }
+        public Vector2<T> yx { get => xy.yx; }
+        public Vector3<T> xzy { get => new Vector3<T>(xy.x, z, xy.y); }
+        public Vector3<T> yxz { get => new Vector3<T>(xy.yx, z); }
+        public Vector3<T> yzx { get => new Vector3<T>(xy.y, z, xy.x); }
+        public Vector3<T> zxy { get => new Vector3<T>(z, xy.x, xy.y); }
+        public Vector3<T> zyx { get => new Vector3<T>(z, xy.yx); }
+
+        public Vector3(T x, T y, T z) : this(new Vector2<T>(x, y), z)
+        {
+            
+        }
+
+        public Vector3(T x, Vector2<T> yz) : this(new Vector2<T>(x, yz.x), yz.y) // yz.x is the y component and yz.y is the z component
+        {
+
+        }
+
+        public Vector3(Vector2<T> xy, T z)
+        {
+            this.xy = xy;
+            this.z = z;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public bool Equals(Vector3<T> other)
+        {
+            return this == other;
+        }
+
+        public static bool operator ==(Vector3<T> left, Vector3<T> right) => left.xy == right.xy && left.z.Equals(right.z);
+        public static bool operator !=(Vector3<T> left, Vector3<T> right) => left.xy != right.xy || !left.z.Equals(right.z);
+    }
+}
