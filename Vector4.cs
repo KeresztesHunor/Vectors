36e7f57795a8bae233cd17a17110ee32bbfe6b4a
kereszteshunor <kereszteshunor36@gmail.com>
2024-01-09 13:07:49 +0100

    \n

--- Vector4.cs
+++ Vector4.cs
@@ -9,6 +9,7 @@ namespace Vectors
         public T y { get => xyz.y; }
         public T z { get => xyz.z; }
         public T w { get; }
+        
         public Vector3<T> xyz { get; }
 
         public Vector4(T x, T y, T z, T w) : this(new Vector3(x, y, z), w)
